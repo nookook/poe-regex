6ee54b6e7138e945644b42aa7cd1e6a18659414f
HB <super_hbr@hotmail.com>
2025-12-23 12:34:14 -0500

    new

--- src/context/FilterContext.tsx
+++ src/context/FilterContext.tsx
@@ -1,6 +1,6 @@
 import { createContext, useState, useContext, useEffect } from "react";
 
-export const useFilterContext = ()
+export const useFilterContext = () => {};
 
 export default function FilterContext() {
   const [filters, setFilters] = useState();
