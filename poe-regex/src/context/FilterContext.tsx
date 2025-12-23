d2b09c44c9721a39869075d58536103161775178
HB <super_hbr@hotmail.com>
2025-12-23 12:24:41 -0500

    new

---
+++ poe-regex/src/context/FilterContext.tsx
@@ -0,0 +1,7 @@
+import { createContext, useState, useContext, useEffect } from "react";
+
+export const useFilterContext = ()
+
+export default function FilterContext() {
+  const [filters, setFilters] = useState();
+}
