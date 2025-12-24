1661b609bf921463b63f80e1f8eb8cc1f9670a40
HB <super_hbr@hotmail.com>
2025-12-24 16:06:20 -0500

    added header title

--- src/pages/MapRegex.tsx
+++ src/pages/MapRegex.tsx
@@ -5,6 +5,14 @@ import "../css/style.css";
 export default function MapRegex() {
   return (
     <main className="w-full">
+      <h1 className="text-4xl mb-6 text-left">
+        <img
+          className="inline h-12 mr-3"
+          src="https://www.poewiki.net/images/9/9c/Mirror_of_Kalandra_inventory_icon.png"
+        />
+        PoE Map Modifier Regex Generator
+        <div className="border mt-1"></div>
+      </h1>
       <FilterProvider>
         <FilterCardList />
       </FilterProvider>
